3fa83a459c341b322c003918f784d180d43bc407
Tokyo <ahmed.tokyo1@gmail.com>
2017-10-15 02:56:00 +0200

    add basic mocks for more queuing funcs

--- src/services/Search/queuingFunctions.js
+++ src/services/Search/queuingFunctions.js
@@ -9,6 +9,7 @@ export const breadthFirstQueuingFunc: QueuingFunction = (
   nodes: Array<Node>,
   newNodes: Array<Node>
 ): Array<Node> => nodes.concat(newNodes);
+
 /**
  * Depth First search Queuing function enques at the front.
  */
@@ -16,3 +17,14 @@ export const depthFirstQueuingFunc: QueuingFunction = (
   nodes: Array<Node>,
   newNodes: Array<Node>
 ): Array<Node> => newNodes.concat(nodes);
+
+/**
+ * @TODO Uniform Cost search Queuing function enques ...
+ */
+export const uniformCostQueuingFunc: QueuingFunction = (
+  nodes: Array<Node>,
+  newNodes: Array<Node>
+): Array<Node> => [];
+
+/** @TODO Greedy, 2 heuristics */
+/** @TODO A*, 2 heuristics */
