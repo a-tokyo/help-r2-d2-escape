984733cc2f603dfa52ca7b4657a2e8d4ea071332
Tokyo <ahmed.tokyo1@gmail.com>
2017-10-15 05:13:32 +0200

    enhance code

--- src/services/Search/Search.js
+++ src/services/Search/Search.js
@@ -31,11 +31,11 @@ export const generalSearch = (
   return null;
 };
 
-/** Backtrack from a node and get a list of operators from the root to reach that node.
+/** BacktrackOperators from a node and get a list of operators from the root to reach that node.
  *
  * if the node has no parent -> it is the root => return a [];
- * if the node has a parent -> backtrack and and get the list of operators to the previous node
+ * if the node has a parent -> backtrackOperators and and get the list of operators to the previous node
  * => return the list of operators to the prev node concatinated with the current node's operator
  */
-export const backTrack = (node: Node): Array<Operator> =>
-  node.parent ? backTrack(node.parent).concat(node.operator) : [];
+export const backTrackOperators = (node: Node): Array<Operator> =>
+  node.parent ? backTrackOperators(node.parent).concat(node.operator) : [];
