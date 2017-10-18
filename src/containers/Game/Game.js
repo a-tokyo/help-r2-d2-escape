3aeaab86d6ac20140e51f185b4e2325fd06cdb72
Tokyo <ahmed.tokyo1@gmail.com>
2017-10-18 11:43:01 +0200

    add ability to change search type

--- src/containers/Game/Game.js
+++ src/containers/Game/Game.js
@@ -1,5 +1,6 @@
+/* @flow weak */
 import React, { Component } from 'react';
-import { Button } from 'reactstrap';
+import { Button, Input } from 'reactstrap';
 
 import { GridUI } from '../../components';
 import { generateGrid, HelpR2D2Search } from '../../services';
@@ -9,11 +10,23 @@ import { solvableLongGrid } from '../../services/generateGrid/testGrids';
 
 import './Game.css';
 
+const searchTypes: Array<string> = [
+  'BF',
+  'DF',
+  'UC',
+  'ID',
+  'GR1',
+  'GR2',
+  'AS1',
+  'AS2',
+];
+
 export default class Game extends Component {
   state = {
     gameGrid: solvableLongGrid,
     onGoingGameGridGrid: null,
     currOnGoingSearchState: null,
+    searchTypeInputValue: 'BF',
   };
 
   componentDidMount() {
@@ -28,17 +41,33 @@ export default class Game extends Component {
     console.info('gameGridMap ==>');
     console.info(gridMapToString(gameGrid.grid));
 
-    console.info('HelpR2D2Search result ==>', HelpR2D2Search(gameGrid));
+    console.info(
+      'HelpR2D2Search result ==>',
+      HelpR2D2Search(gameGrid, this.state.searchTypeInputValue)
+    );
+  };
+
+  _handleChangeSearchTypeInputValue = event => {
+    this.setState({ searchTypeInputValue: event.target.value });
   };
 
   render() {
-    const { gameGrid } = this.state;
+    const { gameGrid, searchTypeInputValue } = this.state;
     return (
       <div>
         <div className="game__controlers">
           <Button color="primary" onClick={this._newGame}>
             New Game
           </Button>
+          <div className="game__controlers_select">
+            <Input
+              type="select"
+              value={searchTypeInputValue}
+              onChange={this._handleChangeSearchTypeInputValue}
+            >
+              {searchTypes.map(type => <option key={type}>{type}</option>)}
+            </Input>
+          </div>
         </div>
         <h3>Initial Grid</h3>
         <GridUI gridInfo={gameGrid} />
