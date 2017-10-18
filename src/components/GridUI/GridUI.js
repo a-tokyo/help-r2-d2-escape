c8ebd17b80ff8cd63d3c3f594ec711afb647773f
Tokyo <ahmed.tokyo1@gmail.com>
2017-10-18 10:51:47 +0200

    enhance ui

--- src/components/GridUI/GridUI.js
+++ src/components/GridUI/GridUI.js
@@ -1,45 +1,86 @@
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
+/* @flow */
+import React from 'react';
 
 import { R2D2Player, StormtrooperObstacle, Teleporter, Rock } from '../';
 
 import './GridUI.css';
 
-export default class componentName extends Component {
-  static propTypes = {
-    grid: PropTypes.arrayOf(PropTypes.arrayOf(PropTypes.object)),
-  };
-  static defaultProps = {
-    grid: [[]],
-  };
-
-  render() {
-    const { grid } = this.props;
-    return (
-      <section className="grid-wrapper">
-        <div className="grid">
-          <div className="grid-row">
-            <div className="grid-cell">
-              <R2D2Player />
-            </div>
-            <div className="grid-cell">b</div>
-            <div className="grid-cell">c</div>
-            <div className="grid-cell">d</div>
-          </div>
-          <div className="grid-row">
-            <div className="grid-cell grid-cell--pressure-pad">a</div>
-            <div className="grid-cell grid-cell--pressure-pad--active">
-              <Rock />
-            </div>
-            <div className="grid-cell">
-              <Teleporter />
-            </div>
-            <div className="grid-cell">
-              <StormtrooperObstacle />
-            </div>
-          </div>
+const GridUI = ({
+  gridInfo,
+}: {
+  gridInfo: { grid: Array<Array<any>>, config: GridConfigObject },
+}) => (
+  <section className="grid-wrapper">
+    <div className="grid">
+      {gridInfo.grid.map((row, rowIndex) => (
+        <div className="grid-row" key={`row_${rowIndex}`}>
+          {gridInfo.grid[rowIndex].map((col, colIndex) => {
+            if (gridInfo.grid[rowIndex][colIndex]) {
+              switch (gridInfo.grid[rowIndex][colIndex].items[0].type) {
+                case 'player':
+                  return (
+                    <div
+                      className="grid-cell"
+                      key={`cell_${rowIndex}-${colIndex}`}
+                    >
+                      <R2D2Player />
+                    </div>
+                  );
+                case 'teleportal':
+                  return (
+                    <div
+                      className="grid-cell"
+                      key={`cell_${rowIndex}-${colIndex}`}
+                    >
+                      <Teleporter />
+                    </div>
+                  );
+                case 'obstacle':
+                  return (
+                    <div
+                      className="grid-cell"
+                      key={`cell_${rowIndex}-${colIndex}`}
+                    >
+                      <StormtrooperObstacle />
+                    </div>
+                  );
+                case 'pressurepad':
+                  return (
+                    <div
+                      className="grid-cell grid-cell--pressure-pad"
+                      key={`cell_${rowIndex}-${colIndex}`}
+                    />
+                  );
+                case 'rock':
+                  return (
+                    <div
+                      className="grid-cell"
+                      key={`cell_${rowIndex}-${colIndex}`}
+                    >
+                      <Rock />
+                    </div>
+                  );
+                default:
+                  return (
+                    <div
+                      className="grid-cell"
+                      key={`cell_${rowIndex}-${colIndex}`}
+                    />
+                  );
+              }
+            } else {
+              return (
+                <div
+                  className="grid-cell"
+                  key={`cell_${rowIndex}-${colIndex}`}
+                />
+              );
+            }
+          })}
         </div>
-      </section>
-    );
-  }
-}
+      ))}
+    </div>
+  </section>
+);
+
+export default GridUI;
--- src/containers/Game/Game.js
+++ src/containers/Game/Game.js
@@ -9,130 +9,11 @@ import { solvableLongGrid } from '../../services/generateGrid/testGrids';
 
 import './Game.css';
 
-const dummyWorkingLongGrid = {
-  grid: [
-    [
-      null,
-      null,
-      null,
-      null,
-      {
-        items: [
-          {
-            type: 'teleportal',
-            name: 'teleportal',
-          },
-        ],
-        position: {
-          row: 0,
-          col: 4,
-        },
-      },
-    ],
-    [
-      {
-        items: [
-          {
-            type: 'pressurepad',
-            name: 'pressurepad_0',
-          },
-        ],
-        position: {
-          row: 1,
-          col: 0,
-        },
-      },
-      null,
-      null,
-      null,
-      null,
-    ],
-    [null, null, null, null, null],
-    [
-      null,
-      {
-        items: [
-          {
-            type: 'rock',
-            name: 'rock_0',
-          },
-        ],
-        position: {
-          row: 3,
-          col: 1,
-        },
-      },
-      null,
-      null,
-      {
-        items: [
-          {
-            type: 'player',
-            name: 'player',
-          },
-        ],
-        position: {
-          row: 3,
-          col: 4,
-        },
-      },
-    ],
-    [
-      null,
-      null,
-      null,
-      null,
-      {
-        items: [
-          {
-            type: 'obstacle',
-            name: 'obstacle_0',
-          },
-        ],
-        position: {
-          row: 4,
-          col: 4,
-        },
-      },
-    ],
-  ],
-  config: {
-    rows: 5,
-    cols: 5,
-    availableCellsCount: 20,
-    playerPosition: {
-      row: 3,
-      col: 4,
-    },
-    teleportalPosition: {
-      row: 0,
-      col: 4,
-    },
-    obstaclesPositions: [
-      {
-        row: 4,
-        col: 4,
-      },
-    ],
-    rocksPositions: [
-      {
-        row: 3,
-        col: 1,
-      },
-    ],
-    pressurePadsPositions: [
-      {
-        row: 1,
-        col: 0,
-      },
-    ],
-    rocksCount: 1,
-    pressurePadsCount: 1,
-    obstaclesCount: 1,
-  },
-};
-
 export default class Game extends Component {
+  state = {
+    gameGrid: solvableLongGrid,
+  };
+
   componentDidMount() {
     // this._newGame();
   }
@@ -149,6 +30,7 @@ export default class Game extends Component {
   };
 
   render() {
+    const { gameGrid } = this.state;
     return (
       <div>
         <div className="game__controlers">
@@ -156,7 +38,7 @@ export default class Game extends Component {
             New Game
           </Button>
         </div>
-        <GridUI />
+        <GridUI gridInfo={gameGrid} />
         <hr />
       </div>
     );
