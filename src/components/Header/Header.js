9b38bfd61cfac94b8b0e15557fe3f36242b2b819
Tokyo <ahmed.tokyo1@gmail.com>
2017-10-19 02:43:46 +0200

    move all UI to components

---
+++ src/components/Header/Header.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import logo from './r2d2_icon.png';
+import './Header.css';
+
+const Header = () => (
+  <header className="App-header">
+    <img src={logo} className="App-logo" alt="logo" />
+    <h1 className="App-title">Help R2-D2 Escape!</h1>
+    <h4>
+      An informative visualization of the different search types used by AI
+      agents.
+    </h4>
+  </header>
+);
+
+export default Header;
