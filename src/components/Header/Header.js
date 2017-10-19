14ad2c61602e162ab24243bd12ea8c38c1a253c4
Tokyo <ahmed.tokyo1@gmail.com>
2017-10-19 02:44:53 +0200

    move all UI to components

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,7 +4,7 @@ import logo from './r2d2_icon.png';
 import './Header.css';
 
 const Header = () => (
-  <header className="App-header">
+  <header className="App-header text-center">
     <img src={logo} className="App-logo" alt="logo" />
     <h1 className="App-title">Help R2-D2 Escape!</h1>
     <h4>
